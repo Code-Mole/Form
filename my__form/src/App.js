eb35f65a4dca4b241d096e4e39f86501833fe918
Asangabia Mole <moleasangabia23@gmail.com>
2024-06-04 23:54:47 +0000

    Header component and syles

--- my__form/src/App.js
+++ my__form/src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Components/Header.js";
+import Form from "./Components/Form.js";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+        <Form />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ my__form/src/Components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div className="form">
+      <div className="container">
+        <form></form>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
