132d30539b3274cb5f27abc872b5c0f0ed2e01c9
Asangabia Mole <moleasangabia23@gmail.com>
2024-06-05 00:22:09 +0000

    form component and styles

--- my__form/src/Components/Form.js
+++ my__form/src/Components/Form.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "./Header.css";
 
 function Form() {
   return (
     <div className="form">
       <div className="container">
-        <form></form>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
